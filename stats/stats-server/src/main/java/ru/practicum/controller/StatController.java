920c1d7261bdf4357b8f3149a72fa806f467a24a
nikolaimurtazin <murtazinnikol@yandex.ru>
2024-12-18 12:12:39 +0100

    "feat: выполнил 1 этап дипломного проекта"

---
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatController.java
@@ -0,0 +1,66 @@
+package ru.practicum.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.EndpointHitDTO;
+import ru.practicum.StatsParams;
+import ru.practicum.ViewStatsDTO;
+import ru.practicum.exeption.WrongDateException;
+import ru.practicum.service.StatService;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+
+    private final StatService statService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EndpointHitDTO saveStats(@Valid @RequestBody
+                                    EndpointHitDTO hitDto) {
+        return statService.save(hitDto);
+    }
+
+
+    @GetMapping("/stats")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ViewStatsDTO> getStats(@NotNull @RequestParam("start") String start,
+                                       @NotNull @RequestParam("end") String end,
+                                       @RequestParam(value = "uris", required = false, defaultValue = "") List<String> uris,
+                                       @RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String decodedStartDate = URLDecoder.decode(start, StandardCharsets.UTF_8);
+        String decodedEndDate = URLDecoder.decode(end, StandardCharsets.UTF_8);
+        LocalDateTime startDecode = LocalDateTime.parse(decodedStartDate, formatter);
+        LocalDateTime endDecode = LocalDateTime.parse(decodedEndDate, formatter);
+        validDate(startDecode, endDecode);
+
+        StatsParams statsParams = StatsParams.builder()
+                .start(startDecode)
+                .end(endDecode)
+                .unique(unique)
+                .uris(uris)
+                .build();
+        return statService.getStats(statsParams);
+    }
+
+    private void validDate(LocalDateTime rangeStart, LocalDateTime rangeEnd) {
+        if (rangeEnd.isBefore(rangeStart)) {
+            throw new WrongDateException("Range end must be after range start");
+        }
+    }
+}
